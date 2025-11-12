313899300e747d0366cff61244a65b96cbc0d268
MuhammadHusnain <muhammad.husnain@paysyslabs.com>
2025-11-12 12:29:04 +0500

    Initial commit

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.testProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hasnain");
+    }
+}
